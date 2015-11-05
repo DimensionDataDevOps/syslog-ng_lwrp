496daefbf7c17376129a97fe803fc5253c09ff5e
Jason McNew <foonix@yahoo.com>
2015-11-05 10:48:01 -0800

    Update metadata with maintainer info and supported platforms.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
 name             'syslog-ng'
-maintainer       'Artem Veremey'
-maintainer_email 'artem@veremey.net'
+maintainer       'Jason McNew'
+maintainer_email 'foonix@yahoo.com'
 license          'Apache 2.0'
 description      'Installs/Configures syslog-ng'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
 
 depends 'yum-epel', '~> 0.6.3'
+
+supports 'centos', '~> 6.0'
