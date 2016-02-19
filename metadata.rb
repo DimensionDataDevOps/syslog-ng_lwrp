6b077478f4a4c5582f4f357fe36e7786d7656b4b
Jason McNew <foonix@yahoo.com>
2016-02-19 07:56:44 -0800

    Bump version to 2.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'foonix@yahoo.com'
 license          'Apache 2.0'
 description      'Installs/Configures syslog-ng'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.1'
+version          '2.0.2'
 
 supports 'centos', '~> 6.0'
