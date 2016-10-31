ea82cdbb513f848d40bbdf6d33fa3e238652e673
Jason McNew <foonix@yahoo.com>
2016-10-31 09:45:55 -0700

    Version bump.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'foonix@yahoo.com'
 license          'Apache 2.0'
 description      'Installs/Configures syslog-ng'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.2'
+version          '2.0.3'
 issues_url       'https://github.com/DimensionDataDevOps/syslog-ng_lwrp/issues'
 source_url       'https://github.com/DimensionDataDevOps/syslog-ng_lwrp'
 
